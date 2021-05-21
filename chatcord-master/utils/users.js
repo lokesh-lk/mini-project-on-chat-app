4dcfc36b68ce7ab1325dacb6ddb8a797f9a3b535
lokeshlk <lokesh010198@outlook.com>
2021-05-21 13:10:58 +0530

    First commit

---
+++ chatcord-master/utils/users.js
@@ -0,0 +1,37 @@
+const users = [];
+
+// Join user to chat
+function userJoin(id, username, room) {
+  const user = { id, username, room };
+  //console.log('User joined the room',user);  //Join the room
+  users.push(user);
+
+  return user;
+}
+
+// Get current user
+function getCurrentUser(id) {
+  return users.find(user => user.id === id);
+}
+
+// User leaves chat
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+  console.log('User leave the Room');  //Leave the room
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+// Get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
+}
+
+module.exports = {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+};
